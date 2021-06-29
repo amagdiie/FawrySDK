35d1f9c1f587c733a6136fd61c8f545f924dff3c
amagdiie <a.magdiie@gmail.com>
2021-06-29 15:38:36 +0200

    initial commit

---
+++ FawryIdentification/src/main/java/com/fawry/identification/MainActivity.java
@@ -0,0 +1,53 @@
+package com.fawry.identification;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonFront = findViewById(R.id.frontId);
+        buttonFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ClassifierActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonBack = findViewById(R.id.backId);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ClassifierActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonFace = findViewById(R.id.faceId);
+        buttonFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonStartScan = findViewById(R.id.startScan);
+        buttonStartScan.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ClassifierActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+
+
+}
